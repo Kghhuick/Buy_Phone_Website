12dde028a820a46adeca130112722bd7af484542
Kamil Ciaston <Kamilciaston6@gmail.com>
2017-08-04 15:47:08 +0200

    next sections and jquery test

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 $(function()	{
 
+var Divs = $(".small_box_phones");
+console.log(Divs);
+Divs.on("mouseenter",function(){
+  console.log($(this));
+ $(".visible").css("visibility","hidden");
+ $(".hidden").css({"visibility":"visible",
+                   "margin-top":"-415px",
+                    
+                   "height":"450px;"
+                                 });
+});
 
-  
+
+
 });
